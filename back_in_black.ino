b3038c3cee1418dfc3848a4dd34ec32f4da8b236
JoshIsDumb4 <82163280+JoshIsDumb4@users.noreply.github.com>
2021-04-24 10:49:04 +0100

    Added bridge section on A string.
    Should now be complete

--- back_in_black.ino
+++ back_in_black.ino
@@ -11,11 +11,26 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int notes[] = {329, 587, 554, 493, 554, 391, 493, 440, 493, 466, 493, 493};
+  int notes[] = {164, 293, 277, 246, 277, 195, 246, 220, 246, 233, 246, 246};
+  int notes2[] = {220, 391, 369, 329, 369, 261, 329, 293, 329, 311, 329, 329};
   int times[] = {163, 163, 163, 163, 163, 326, 163, 326, 163, 326, 163, 326};
 int n = 0;
+int x = 0;
+while (x < 3){
 while (n < 12){
   tone(pin,notes[n],times[n],0);
   n=n+1;
+  }
+x=x+1;
+n=0;
+}
+x=0;
+while (x < 1){
+while (n < 12){
+  tone(pin,notes2[n],times[n],0);
+  n=n+1;
+}
+x=x+1;
+n=0;
 }
 }
