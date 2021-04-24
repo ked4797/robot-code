a334a2e79a520a5f5ba4c3e0eda8216942b13a9a
JoshIsDumb4 <82163280+JoshIsDumb4@users.noreply.github.com>
2021-04-24 10:37:36 +0100

    pin is not 8... duh

--- back_in_black.ino
+++ back_in_black.ino
@@ -13,7 +13,6 @@ void loop() {
   // put your main code here, to run repeatedly:
   int notes[] = {329, 587, 554, 493, 554, 391, 493, 440, 493, 466, 493, 493};
   int times[] = {163, 163, 163, 163, 163, 326, 163, 326, 163, 326, 163, 326};
-int pin=8;
 int n = 0;
 while (n < 12){
   tone(pin,notes[n],times[n],0);
