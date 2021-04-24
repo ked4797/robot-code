25404b0ba4c3deaf72125f453fe1ff3633183581
JoshIsDumb4 <82163280+JoshIsDumb4@users.noreply.github.com>
2021-04-24 10:31:38 +0100

    Should work now

--- back_in_black.ino
+++ back_in_black.ino
@@ -1,17 +1,22 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Tone32.h>
 
+#define pin 32
+#define BUZZER_CHANNEL 0
+
+void setup() {
+  pinMode(26, OUTPUT);
+  digitalWrite(26, HIGH);
+  // put your setup code here, to run once:
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int notes[] = {493, 440, 164, 293, 277, 246, 277, 195, 246, 195, 220, 246, 233};
+  int notes[] = {329, 587, 554, 493, 554, 391, 493, 440, 493, 466, 493, 493};
+  int times[] = {163, 163, 163, 163, 163, 326, 163, 326, 163, 326, 163, 326};
 int pin=8;
 int n = 0;
-while (n < 16){
-  tone(pin,notes[n],100);
-  delay(110);
+while (n < 12){
+  tone(pin,notes[n],times[n],0);
   n=n+1;
 }
 }
-// This is very broken
